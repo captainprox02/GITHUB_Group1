96077e4b7873f132b1357e54216e7959ad3422c5
Smith7402 <tuandat.nguyenphan.07042002@gmail.com>
2022-03-16 16:17:09 +0700

    multi2numbers
    commit

---
+++ Multi2Number/src/mutil2number/Mutil2Number.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mutil2number;
+
+/**
+ *
+ * @author Dat
+ */
+public class Mutil2Number {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application app = new Application();
+        app.show();
+    }
+    
+}
